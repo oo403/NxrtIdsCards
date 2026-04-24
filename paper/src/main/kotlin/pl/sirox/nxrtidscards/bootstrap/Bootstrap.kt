cf9a84f69b1f5f2b9deb0a93239f22e993e75057
403 <oo403.kontakt@gmail.com>
2026-04-24 18:40:31 +0200

    feat: bootstrap logger

--- paper/src/main/kotlin/pl/sirox/nxrtidscards/bootstrap/Bootstrap.kt
+++ paper/src/main/kotlin/pl/sirox/nxrtidscards/bootstrap/Bootstrap.kt
@@ -1,16 +1,27 @@
 package pl.sirox.nxrtidscards.bootstrap
 
 import com.google.inject.Guice
+import com.google.inject.Injector
 import org.bukkit.plugin.java.JavaPlugin
+import org.slf4j.Logger
+import pl.sirox.common.logging.LoggerFactory
+import pl.sirox.common.logging.logger
 import pl.sirox.common.module.ConfigurationModule
 
 class Bootstrap : JavaPlugin() {
 
+    private lateinit var injector: Injector
+    private lateinit var loggerFactory: LoggerFactory
+    private lateinit var logger: Logger
+
     override fun onLoad() {
         try {
-            Guice.createInjector(
+            injector = Guice.createInjector(
                 ConfigurationModule(this.dataFolder)
             )
+
+            loggerFactory = injector.getInstance(LoggerFactory::class.java)
+            logger = loggerFactory.logger<Bootstrap>()
 
         } catch (e: Exception) {
             e.printStackTrace()
@@ -18,10 +29,12 @@ class Bootstrap : JavaPlugin() {
     }
 
     override fun onEnable() {
-
+        logger.info("NxtrIdCards is starting...")
+        logger.info("NxtrIdCards is enabled!")
     }
 
     override fun onDisable() {
+        logger.info("NxtrIdCards is disabled!")
     }
 
 }
