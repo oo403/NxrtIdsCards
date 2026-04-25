d1a0e435e1c9162e8edc01587b37862fb914162d
403 <oo403.kontakt@gmail.com>
2026-04-25 17:23:38 +0200

    feat: missing permissions handler

---
+++ paper/src/main/kotlin/pl/sirox/nxrtidscards/handler/MissingPermissionHandler.kt
@@ -0,0 +1,28 @@
+package pl.sirox.nxrtidscards.handler
+
+import com.google.inject.Inject
+import dev.rollczi.litecommands.handler.result.ResultHandlerChain
+import dev.rollczi.litecommands.invocation.Invocation
+import dev.rollczi.litecommands.permission.MissingPermissions
+import dev.rollczi.litecommands.permission.MissingPermissionsHandler
+import org.bukkit.command.CommandSender
+import pl.sirox.common.configuration.MessagesConfiguration
+import pl.sirox.common.service.MultificationService
+
+class MissingPermissionHandler @Inject constructor(
+    private val messagesConfiguration: MessagesConfiguration,
+    private val multificationService: MultificationService
+) : MissingPermissionsHandler<CommandSender> {
+
+    override fun handle(
+        invocation: Invocation<CommandSender?>?,
+        missingPermissions: MissingPermissions?,
+        chain: ResultHandlerChain<CommandSender?>?
+    ) {
+        multificationService.create()
+            .viewer(invocation!!.sender())
+            .notice(messagesConfiguration.missingPermissionsMessage)
+            .send()
+    }
+
+}
